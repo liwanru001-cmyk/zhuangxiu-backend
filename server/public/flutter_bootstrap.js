f61d5bab8d4fd991ee2150a822c00994c6f3eec4
舒福德 <shufude@shufudedeiMac.local>
2026-06-28 17:20:51 +0800

    Show notification summary in messages

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3448831940" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2721887165" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
