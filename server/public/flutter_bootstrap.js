477d80fc3e14188601d3e56d66132c8f01979af7
舒福德 <shufude@shufudedeiMac.local>
2026-06-28 14:22:55 +0800

    Remove stage item helper text

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4050100239" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3979050276" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
