d79ae208b506b5674f5f11a6cd7202a577db4bc7
舒福德 <shufude@shufudedeiMac.local>
2026-06-28 20:32:11 +0800

    Tighten owner member UI permissions

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3553153122" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2551413168" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
