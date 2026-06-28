d7219ff0a9755420ad695bf195ffbfd200d9dc85
舒福德 <shufude@shufudedeiMac.local>
2026-06-28 17:54:14 +0800

    Add owner-side member UI

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3020416863" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2611752558" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
