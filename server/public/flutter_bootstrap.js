f21bd9248649a8903e8436c9ab555b2bf13b0622
舒福德 <shufude@shufudedeiMac.local>
2026-06-28 17:23:40 +0800

    Always show notification center entry

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2721887165" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3020416863" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
